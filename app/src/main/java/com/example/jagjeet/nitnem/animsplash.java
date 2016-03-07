28914d3c13ef5c82012cf405ee900569737d86f0
Jagjeet Singh <iamjagjeetubhi@gmail.com>
2016-03-07 19:06:15 +0530

    Merge branch 'master' of https://github.com/iamjagjeetubhi/nitnem1
    Conflicts:
    	.gitignore

---
+++ app/src/main/java/com/example/jagjeet/nitnem/animsplash.java
@@ -0,0 +1,55 @@
+package com.example.jagjeet.nitnem;
+
+/**
+ * Created by jagjeet on 7/3/16.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class animsplash extends Activity {
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+
+        /** Making this activity, full screen */
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.animsplash);
+        CountDown _tik;
+        _tik=new CountDown(2500,2500,this,MainActivity.class);// It delay the screen for 2.5 second and after that switch to YourNextActivity
+        _tik.start();
+        StartAnimations();
+    }
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        LinearLayout l=(LinearLayout) findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.logo);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+
+
+    }
+
+    }
+
